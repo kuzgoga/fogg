751a739be3091ae70a8a5381300d4ee7ff15bae0
gogacoder <me@gogacoder.com>
2025-02-25 18:44:42 +0700

    feat: fully tests coverage

--- storage_test.go
+++ storage_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
@@ -37,9 +38,70 @@ func TestParse(t *testing.T) {
 		t.Errorf("unexpected error: %s", err)
 	}
 
+	if !storage.HasTag("gorm") {
+		t.Errorf("`gorm` tag is absont in storage")
+	}
+
 	parsedTag := storage.GetTag("gorm")
 
 	if !reflect.DeepEqual(expectedTag, *parsedTag) {
 		t.Errorf("got: %+v\n, expected: %+v\n", parsedTag, expectedTag)
+	}
+}
+
+func TestParseTagErrors(t *testing.T) {
+	const nonQuotedValueTag string = `gorm:default:value'`
+	expectedError := fmt.Sprintf(nonQuotedValueErr, "gorm")
+	_, err := Parse(nonQuotedValueTag)
+	if err == nil || err.Error() != expectedError {
+		t.Errorf("expected error `%s`, got: %s", expectedError, err)
+	}
+}
+
+func TestParseSubtagErrors(t *testing.T) {
+	const unquotedTag string = `gorm:"default:'value"`
+	const expectedError = unclosedBacktickErr
+	_, err := Parse(unquotedTag)
+	if err == nil || err.Error() != expectedError {
+		t.Errorf("expected error `%s`, got: %s", expectedError, err)
+	}
+}
+
+func TestParseDuplicatedTagsError(t *testing.T) {
+	const duplicatedTags string = `gorm:"not null" gorm:"foreignKey:CustomerId"`
+	expectedError := fmt.Sprintf(duplicatedTagsErr, "gorm")
+	_, err := Parse(duplicatedTags)
+	if err == nil || err.Error() != expectedError {
+		t.Errorf("expected error `%s`, got: %s", expectedError, err)
+	}
+}
+
+func TestParseSplitTagItemsError(t *testing.T) {
+	const invalidTagContent = `gorm:"default:'value`
+	expectedError := unclosedBacktickErr
+	_, err := Parse(invalidTagContent)
+	if err == nil || err.Error() != expectedError {
+		t.Errorf("expected error `%s`, got: %s", expectedError, err)
+	}
+}
+
+func TestGetTagNotFound(t *testing.T) {
+	storage := Storage{
+		tags: make(map[string]Tag),
+	}
+
+	tag := storage.GetTag("nonexistent")
+	if tag != nil {
+		t.Errorf("expected nil, got %v", tag)
+	}
+}
+
+func TestHasTagNotFound(t *testing.T) {
+	storage := Storage{
+		tags: make(map[string]Tag),
+	}
+
+	if storage.HasTag("nonexistent") {
+		t.Errorf("expected false, got true")
 	}
 }
--- tag_test.go
+++ tag_test.go
@@ -1,1 +1,52 @@
 package main
+
+import "testing"
+
+func TestTagMethods(t *testing.T) {
+	tag := Tag{
+		name:    "example",
+		value:   "value",
+		params:  map[string]TagParam{"param1": {Value: "value1"}},
+		options: []string{"option1", "option2"},
+	}
+
+	if tag.Name() != "example" {
+		t.Errorf("expected name to be 'example', got %s", tag.Name())
+	}
+
+	if !tag.HasOption("option1") {
+		t.Errorf("expected to have option 'option1'")
+	}
+
+	if tag.HasOption("option3") {
+		t.Errorf("expected not to have option 'option3'")
+	}
+
+	if !tag.HasParam("param1") {
+		t.Errorf("expected to have param 'param1'")
+	}
+
+	if tag.HasParam("param2") {
+		t.Errorf("expected not to have param 'param2'")
+	}
+
+	if param := tag.GetParam("param1"); param == nil || param.Value != "value1" {
+		t.Errorf("expected to get param 'param1' with value 'value1'")
+	}
+
+	if param := tag.GetParam("param2"); param != nil {
+		t.Errorf("expected to get nil for param 'param2'")
+	}
+
+	if value := tag.GetParamOr("param1", "default"); value != "value1" {
+		t.Errorf("expected to get 'value1' for param 'param1'")
+	}
+
+	if value := tag.GetParamOr("param2", "default"); value != "default" {
+		t.Errorf("expected to get 'default' for param 'param2'")
+	}
+
+	if tag.GetValue() != "value" {
+		t.Errorf("expected value to be 'value', got %s", tag.GetValue())
+	}
+}
